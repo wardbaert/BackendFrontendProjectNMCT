00d451d1b835334f8ff06097f85eff7f14ff3b1d
SiebertSchoutteten <siebertschoutteten@hotmail.com>
2016-12-29 04:55:44 +0100

    feat: updated mongoDB user model scheme

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 module.exports = {
-	//'url' : 'mongodb://serieadmin:admin123@ds119718.mlab.com:19718/seriecheckerdb'
-	'url' : 'mongodb://localhost/serieChecker' 
+    'url': 'mongodb://serieadmin:admin123@ds119718.mlab.com:19718/seriecheckerdb'
+        //'url' : 'mongodb://localhost/serieChecker' 
 }
--- models/user.js
+++ models/user.js
@@ -1,10 +1,18 @@
 var mongoose = require('mongoose');
 
-module.exports = mongoose.model('User',{
-	id: String,
-	username: String,
-	password: String,
-	email: String,
-	firstName: String,
-	lastName: String
+module.exports = mongoose.model('User', {
+    id: String,
+    username: String,
+    password: String,
+    email: String,
+    firstName: String,
+    lastName: String,
+    Series: [{
+        id: Number,
+        EpisodesWatched: [{
+            id: Number,
+            seasonid: Number,
+            skipped: Boolean
+        }]
+    }]
 });
