bc9f444aecb2057565c9652ce38be909f601830f
SiebertSchoutteten <siebertschoutteten@hotmail.com>
2017-01-07 20:15:39 +0100

    chore: modified show.js

--- data/models/show.js
+++ data/models/show.js
@@ -20,21 +20,21 @@ var MovieDB = require('moviedb')('bddb2ffe821b6b28acf4db374f786183');
 
 showrepo = (function() {
     var getSeries = function(next) {
-        MovieDB.miscPopularTvs( function(err, serie) {
+        MovieDB.miscPopularTvs(function(err, serie) {
             //console.log(err, serie);
             next(err, serie);
         });
     };
 
-    var getSeriesById = function(id, next){
-        MovieDB.tvInfo({id: id}, function(err, serie) {
+    var getSeriesById = function(id, next) {
+        MovieDB.tvInfo({ id: id }, function(err, serie) {
             console.log(err, serie);
             next(err, serie);
         });
     }
 
-    var getSeasonById = function(sid, seid, next){
-     MovieDB.tvSeasonInfo({season_number: seid, id: sid},function(err, serie){
+    var getSeasonById = function(sid, seid, next) {
+        MovieDB.tvSeasonInfo({ season_number: seid, id: sid }, function(err, serie) {
             console.log(serie);
             next(err, serie);
         });
